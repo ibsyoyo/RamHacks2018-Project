96c8b4e8d79149e12c36bebde8167c99dc73e1ba
Andy Huynh <johnnyishere21@yahoo.com>
2018-09-23 04:03:23 -0400

    transition from login to search

--- zias/src/Login.js
+++ zias/src/Login.js
@@ -1,6 +1,9 @@
 $("#login-button").click(function(event){
     event.preventDefault();
+    console.log("poop");
+// $('form').fadeOut(500);
+// $('.wrapper').addClass('form-success');
 
-$('form').fadeOut(500);
-$('.wrapper').addClass('form-success');
+window.location.replace("ibm.html")
+
 });
