08543af0cd3ac06347771df167a866e720b3cfc2
Andy Huynh <johnnyishere21@yahoo.com>
2018-09-23 06:53:00 -0400

    getting database to work

--- zias/src/index.js
+++ zias/src/index.js
@@ -1,9 +1,12 @@
 import { Stitch, RemoteMongoClient, AnonymousCredential } from 'mongodb-stitch-browser-sdk';
 
-let appId = 'zias-atmil'
+// let appId = 'zias-atmil';
+// const client = Stitch.initializeDefaultAppClient(appId);
+
+// const mdb = client.getServiceClient(RemoteMongoClient.factory, 'mongob-atlas');
 
 function initializeAndLogin() {
-  const client = Stitch.initializeDefaultAppClient('zias-atmil');
+  const client = Stitch.initializeDefaultAppClient("zias-atmil");
   client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
     document.getElementById('auth-status').innerHTML = 
       `Logged in as anonymous user with id ${user.id}`;
